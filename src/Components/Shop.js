b0f36736b896933b6f1927e548065ef8cd6a6ed1
Subham Raj <glorious10info@gmail.com>
2023-07-04 11:31:29 +0530

    made actionCreators, reducers inside state folder

--- src/Components/Shop.js
+++ src/Components/Shop.js
@@ -3,9 +3,9 @@ import React from 'react'
 const Shop = () => {
   return (
     <div>
-        <h2>Buy Adidas shoes: Rs50</h2>
+        <h2>Deposit/Withdraw Money</h2>
         <button className="btn btn-primary mx-2 my-4">-</button>
-            Add to Cart
+            Update Balance
         <button className="btn btn-primary mx-2">+</button>
     </div>
   )
