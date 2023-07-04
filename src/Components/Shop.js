5c528bff6180899836b9a601be9efc727e442884
Subham Raj <glorious10info@gmail.com>
2023-07-04 07:47:45 +0530

    Redux App creation started

---
+++ src/Components/Shop.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Shop = () => {
+  return (
+    <div>
+        <h2>Buy Adidas shoes: Rs50</h2>
+        <button className="btn btn-primary mx-2 my-4">-</button>
+            Add to Cart
+        <button className="btn btn-primary mx-2">+</button>
+    </div>
+  )
+}
+
+export default Shop
